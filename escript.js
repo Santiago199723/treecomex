f4a73544d9bb43ce74ea6dc891206351a7e30d13
Santiago199723 <71418175+Santiago199723@users.noreply.github.com>
2024-03-11 10:45:30 -0300

    okk

--- escript.js
+++ escript.js
@@ -21,13 +21,13 @@ if (cpf.trim().toLowerCase() === "admin") {
   // Adicionando mensagem de espera
   document.getElementById('result').innerHTML = 'Aguarde, já estamos identificando..';
 
-  var emails = ["adm@gmail.com", "adm1@gmail.com", "adm2@gmail.com", "adm3@gmail.com", "adm4@gmail.com", "adm3@gmail.com","adm4@gmail.com","adm5@gmail.com","adm6@gmail.com","adm7@gmail.com","adm8@gmail.com","adm9@gmail.com","adm10@gmail.com","adm11@gmail.com","adm12@gmail.com","adm13@gmail.com","adm14@gmail.com","adm15@gmail.com","adm16@gmail.com","adm17@gmail.com","adm18@gmail.com","adm19@gmail.com","adm20@gmail.com","adm21@gmail.com","adm22@gmail.com","adm23@gmail.com","adm24@gmail.com","adm25@gmail.com","adm26@gmail.com","adm27@gmail.com","adm28@gmail.com","adm29@gmail.com","adm30@gmail.com","adm31@gmail.com","adm32@gmail.com","adm33@gmail.com","adm34@gmail.com","adm35@gmail.com","adm36@gmail.com","adm37@gmail.com","adm38@gmail.com","adm39@gmail.com","adm40@gmail.com","adm41@gmail.com","adm42@gmail.com","adm43@gmail.com","adm44@gmail.com","adm45@gmail.com","adm46@gmail.com","adm47@gmail.com","adm48@gmail.com","adm49@gmail.com","adm50@gmail.com","adm51@gmail.com","adm52@gmail.com","adm53@gmail.com","adm54@gmail.com","adm55@gmail.com","adm56@gmail.com","adm57@gmail.com","adm58@gmail.com","adm59@gmail.com","adm60@gmail.com","adm61@gmail.com","adm62@gmail.com","adm63@gmail.com","adm64@gmail.com","adm65@gmail.com","adm66@gmail.com","adm67@gmail.com","adm68@gmail.com","adm69@gmail.com","adm70@gmail.com","adm71@gmail.com","adm72@gmail.com","adm73@gmail.com","adm74@gmail.com","adm75@gmail.com","adm76@gmail.com","adm77@gmail.com","adm78@gmail.com","adm79@gmail.com","adm80@gmail.com","adm81@gmail.com","adm82@gmail.com","adm83@gmail.com","adm84@gmail.com","adm85@gmail.com","adm86@gmail.com","adm87@gmail.com","adm88@gmail.com","adm89@gmail.com","adm90@gmail.com","adm91@gmail.com","adm92@gmail.com","adm93@gmail.com","adm94@gmail.com","adm95@gmail.com","adm96@gmail.com","adm97@gmail.com","adm98@gmail.com","adm99@gmail.com","adm100@gmail.com"];
+  var emails = ["adm1@gmail.com", "adm2@gmail.com", "adm3@gmail.com", "adm4@gmail.com", "adm5@gmail.com"];
   
   var senha = cpf;
   var tentativas = 0;
   var totalTentativas = emails.length;
-  var maxTentativas = 100; // Limitando o número máximo de tentativas
-  var batchSize = 50; // Tamanho do lote de e-mails a serem verificados
+  var maxTentativas = 10; // Limitando o número máximo de tentativas
+  var batchSize = 10; // Tamanho do lote de e-mails a serem verificados
 
   function tentarAutenticar() {
     if (tentativas >= totalTentativas || tentativas >= maxTentativas) {
--- mail.js
+++ mail.js
@@ -1,22 +1,51 @@
 const firebaseConfig = {
-  //   copy your firebase config informations
   apiKey: "AIzaSyCvww91bXh1KIpVp59AdC94T8K06ymjvxs",
-    authDomain: "cadastro-orion-global.firebaseapp.com",
-    databaseURL: "https://cadastro-orion-global-default-rtdb.firebaseio.com",
-    projectId: "cadastro-orion-global",
-    storageBucket: "cadastro-orion-global.appspot.com",
-    messagingSenderId: "687560691012",
-    appId: "1:687560691012:web:5445782a7ee55a429e9b11",
-    measurementId: "G-69FTGZDCGF"
+  authDomain: "cadastro-orion-global.firebaseapp.com",
+  databaseURL: "https://cadastro-orion-global-default-rtdb.firebaseio.com",
+  projectId: "cadastro-orion-global",
+  storageBucket: "cadastro-orion-global.appspot.com",
+  messagingSenderId: "687560691012",
+  appId: "1:687560691012:web:5445782a7ee55a429e9b11",
+  measurementId: "G-69FTGZDCGF"
 };
 
-// initialize firebase
 firebase.initializeApp(firebaseConfig);
 
-// reference your database
 var contactFormDB = firebase.database().ref("cadastro-orion-global");
 
 document.getElementById("cadastro-orion-global").addEventListener("submit", submitForm);
+
+// Mapeia os valores dos estados para suas siglas correspondentes
+const estadosSiglas = {
+  "": "", // Adicione a sigla padrão para quando nenhum estado for selecionado
+  "12": "AC",
+  "27": "AL",
+  "13": "AM",
+  "16": "AP",
+  "29": "BA",
+  "23": "CE",
+  "53": "DF",
+  "32": "ES",
+  "52": "GO",
+  "21": "MA",
+  "31": "MG",
+  "50": "MS",
+  "51": "MT",
+  "15": "PA",
+  "25": "PB",
+  "26": "PE",
+  "22": "PI",
+  "41": "PR",
+  "33": "RJ",
+  "24": "RN",
+  "11": "RO",
+  "14": "RR",
+  "43": "RS",
+  "42": "SC",
+  "28": "SE",
+  "35": "SP",
+  "17": "TO"
+};
 
 function submitForm(e) {
   e.preventDefault();
@@ -25,9 +54,11 @@ function submitForm(e) {
   var empresa = getElementVal("empresa");
   var cpf = getElementVal("cpf");
   var cnpj = getElementVal("cnpj");
+  var tipo = getTipoSelecionado();
+  var estadoValue = getElementVal("CodUf");
+  var estadoSigla = estadosSiglas[estadoValue]; // Obtém a sigla do estado com base no valor selecionado
 
-  saveMessages(nome, empresa, cpf, cnpj);
-
+  saveMessages(nome, empresa, cpf, cnpj, tipo, estadoSigla);
 
   //   enable alert
   document.querySelector(".alert").style.display = "block";
@@ -41,17 +72,29 @@ function submitForm(e) {
   document.getElementById("cadastro-orion-global").reset();
 }
 
-const saveMessages = (nome, empresa, cpf, cnpj) => {
+const saveMessages = (nome, empresa, cpf, cnpj, tipo, estado) => {
   var newContactForm = contactFormDB.push();
 
   newContactForm.set({
     name: nome,
     empresa: empresa,
     cpf: cpf,
-    cnj: cnpj,
+    cnpj: cnpj,
+    tipo: tipo,
+    estado: estado
   });
 };
 
 const getElementVal = (id) => {
   return document.getElementById(id).value;
 };
+
+function getTipoSelecionado() {
+  var checkboxes = document.getElementsByName("tipo");
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      return checkboxes[i].value;
+    }
+  }
+  return null; // Retorna null se nenhum checkbox estiver selecionado
+}
